1a5ce5791445ad94b29a3d3ae929323f3848f80a
sashamrobinson <sasharobinson235@gmail.com>
2023-07-03 00:46:18 -0400

    Initial project commit

---
+++ Storyteller/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Storyteller
+//
+//  Created by Sasha Robinson on 2023-07-02.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button("Gang") {
+                
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Storyteller/StorytellerApp.swift
@@ -0,0 +1,18 @@
+//
+//  StorytellerApp.swift
+//  Storyteller
+//
+//  Created by Sasha Robinson on 2023-07-02.
+//
+
+import SwiftUI
+
+@main
+struct StorytellerApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            TestView()
+        }
+    }
+}
