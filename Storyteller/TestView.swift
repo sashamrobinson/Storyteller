9ce352cee6a2a06074e56630cf31c0b6cfc37915
sashamrobinson <sasharobinson235@gmail.com>
2023-07-05 18:32:12 -0400

    working on speech to text, text to speech and setting up API key

--- Storyteller/TestView.swift
+++ Storyteller/TestView.swift
@@ -6,12 +6,62 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct TestView: View {
+    
+    // Speech to text
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State var transcript : String = ""
+    @State private var isRecording = false
+    
+    // Text to speech
+    @State private var name : String = ""
+    
+    let synthesizer = AVSpeechSynthesizer()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button("Start transcribing text") {
+                startConversation()
+            }
+            Button("Stop transcribing text") {
+                endConversation()
+            }
+            .padding()
+            Text(transcript)
+            TextField("Name", text: $name)
+            Button("Greet") {
+                let utterance = AVSpeechUtterance(string: "Hello \(name). How are you doing today? I hope your day is going well and that my voice is crystal clear.")
+                utterance.voice = AVSpeechSynthesisVoice(language: "en-AU")
+                utterance.rate = 0.5
+                
+                synthesizer.speak(utterance)
+            }
+            
+        }
+    }
+    
+    private func startConversation() {
+        
+        // Reset speech recognizer, begin transcribing and begin recording
+        speechRecognizer.resetTranscript()
+        speechRecognizer.startTranscribing()
+        isRecording = true
+    }
+    
+    private func endConversation() {
+        
+        // Stop transcribing and stop recording
+        speechRecognizer.stopTranscribing()
+        transcript = speechRecognizer.transcript
+        print("This is working")
+        print(transcript)
+        isRecording = false
     }
 }
+
+
 
 struct TestView_Previews: PreviewProvider {
     static var previews: some View {
